391a6f86109b5063b952c711b0e7aaa1eff57219
JTrojanowicz <jaroslaw.trojanowicz@gmail.com>
2022-07-27 08:58:46 +0200

    Initial Commit

---
+++ Test_SwiftUI_PresentationContainers_Detents/Test_SwiftUI_PresentationContainers_DetentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUI_PresentationContainers_DetentsApp.swift
+//  Test_SwiftUI_PresentationContainers_Detents
+//
+//  Created by Jaroslaw Trojanowicz on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUI_PresentationContainers_DetentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
